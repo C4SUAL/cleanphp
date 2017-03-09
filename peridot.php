0a8d419e45a596e1fa15ad2dad3a14fc4e66af6b
Alan Mitchell <alan.mitchell@flow.co.uk>
2017-03-09 07:55:48 +0000

    Laravel setup

--- peridot.php
+++ peridot.php
@@ -5,8 +5,8 @@ use Peridot\Plugin\Prophecy\ProphecyPlugin;
 use Peridot\Plugin\Watcher\WatcherPlugin;
 
 return function (EventEmitterInterface $emitter) {
-    $watcher = new WatcherPlugin($emitter);
-    $watcher->track(__DIR__ . '/src');
+//    $watcher = new WatcherPlugin($emitter);
+//    $watcher->track(__DIR__ . '/src');
 
     new ProphecyPlugin($emitter);
 };
